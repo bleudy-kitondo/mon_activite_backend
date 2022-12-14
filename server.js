406475a08fbfb2a540b5d0576fb1cecd3c9e10d5
bleudy TETE <bleudykitondo.bk@gmail.com>
2022-12-14 16:16:30 +0100

    refactor: change express to server_express

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require('http')
-const app = require('./express')
+const app = require('./server_express')
 require('dotenv').config()
 require('./connection/mongoose')
 const port = process.env.port
 const server = http.createServer(app)
-
 
 server.listen(port, () => {
   console.log('server started in port: ', port)
